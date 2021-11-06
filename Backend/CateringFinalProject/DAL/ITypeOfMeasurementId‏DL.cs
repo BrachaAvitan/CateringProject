06989a7ba247760eedc793938fcae114adf51835
michal <michalyis144@gmail.com>
2021-11-06 19:21:58 +0200

    add controler typeOfMeasureMent

---
+++ Backend/CateringFinalProject/DAL/ITypeOfMeasurementId‏DL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.Models;
+
+namespace DAL
+{
+    interface ITypeOfMeasurementId‏DL
+    {
+        //קבלת כל סוגי המדידות
+        Task<List<TblTypesOfMeasurements>> GetAllMeasurementsAsync();
+        //קבלת סוג לפי מזהה מדידה
+        Task<TblTypesOfMeasurements> GetMeasurementAsync(int id);
+        //הוספה לטבלת מדידות
+        Task InsertMeasurementsAsync(TblTypesOfMeasurements c);
+        //עדכון סוג מדידה
+        Task UpdateMeasurementsAsync(TblTypesOfMeasurements c);
+        //מחיקת סוג מדידה
+        Task DeleteMeasurementsAsync(int id);
+    }
+}
+
+    
+
